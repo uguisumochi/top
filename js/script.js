a93c7910ba88b0a45dfa4e296a558919481d3360
HOME-PC\megumi <momi.asanoha@gmail.com>
2016-11-11 01:16:16 +0900

    スクロールした後のページ切り替えでページトップに移動するようにした

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ $(function () {
         console.log(e);
         $(e.target.hash).show();
         $(".page-header > h2").contents().replaceWith(e.target.text);
+		$("html,body").animate({
+			scrollTop: 0
+		}, 1500);
     })
-    // $(".btn").click(function () { return false; });
+    $(".btn").click(function () { return false; });
 });
