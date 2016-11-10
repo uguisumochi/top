bb0f0d4a9d7ca0b1e94bb53c6c5e67c095b1b4d6
uguisumochi <momi.asanoha@gmail.com>
2016-11-10 09:45:05 +0900

    色々変更写真追加した

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ $(function (){
         $(".content").hide();
         console.log(e);
         $(e.target.hash).show();
+        $(".page-header > h2").contents().replaceWith(e.target.text);
     })
 });
