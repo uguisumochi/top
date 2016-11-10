98136945053f99822567cf6f017103511f60b575
HOME-PC\megumi <momi.asanoha@gmail.com>
2016-11-11 00:56:14 +0900

    写真増やした

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-$(function (){
-    $(".btn").mouseover(function(e){
+$(function () {
+    $(".btn").mouseover(function (e) {
         $(".content").hide();
         console.log(e);
         $(e.target.hash).show();
         $(".page-header > h2").contents().replaceWith(e.target.text);
     })
+    // $(".btn").click(function () { return false; });
 });
